88a38cc7ad58e44f1c75a706aa507f98b67ff263
OrlandoRNBR <orlandoramosneto@gmail.com>
2026-02-28 18:17:53 -0300

    adição do teste de movimento

--- main.c
+++ main.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main (void){
-    char teste [16][64];
+    char teste [16][64], tirox = 0, tiroy = 8;
 
     for(int i = 0; i < 16;i++){
         for(int j = 0; j < 64; j++){
-            teste [i][j] = '\0';
+            teste [i][j] = ' ';
         }
     }
-    teste [2][26] = 'u';
     printf("Matriz cheia\n");
-    for (int i = 0; i < 16;i++ ){
-        for(int j = 0; j < 64;j++){
-            printf("%c", teste[i][j]);
-        }
-        printf("\n");
-    }
 
+    while(tirox != 64){
+        system("clear");
+        teste [tiroy][tirox] = 'o';
+
+        if(tirox > 0){
+            teste [tiroy][tirox-1]= ' ';
+        }
+    
+        for (int i = 0; i < 16;i++ ){
+            for(int j = 0; j < 64;j++){
+                printf("%c", teste[i][j]);
+            }
+            printf("\n");
+        }
+        printf("valor x = %d\n", tirox);
+        tirox++;
+    
+    }
     return 0;
 }
--- notes.md
+++ notes.md
@@ -3,3 +3,6 @@ Nesse caso eu pretendo programar um jogo estilo rei da pradaria em c com ou sem 
 
 dia 27/02/2026
 Testei uma malha de 16x64 para definir o mapa que por enquanto vai ser no proprio terminal com base me um looping. mas hj vai ser só isso. a ideia é encher de '\0'para o "chão" ser o fundo do terminal e o resto do jogo serem caracteres. Mas o resto fica para outro dia.
+
+dia 28/02/2026
+porgramei uma movimentação simples do "tiro" e coloquei o system("clear") para limpar o terminal a cada frame do jogo agora vou para outro projeto e em outro momento vou continuar.
